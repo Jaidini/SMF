4eaedcc5f56b88b16d3951c345acf0d76831c1cd
Jaidini <romancejaime@gmail.com>
2017-06-06 13:06:46 +0200

    Ya geolocaliza :D
    Signed-off-by: Jaidini <romancejaime@gmail.com>

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -32,6 +32,38 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.Position",
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -40,7 +72,9 @@ module.exports.metadata =
     "cordova-plugin-dialogs": "1.3.3",
     "cordova-plugin-network-information": "1.3.3",
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-sqlite-storage": "2.0.4"
+    "cordova-sqlite-storage": "2.0.4",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-geolocation": "2.4.3"
 };
 // BOTTOM OF METADATA
 });
--- www/js/pantallaPrincipal.js
+++ www/js/pantallaPrincipal.js
@@ -1,6 +1,9 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 var db = null;
+var user = '';
+var latitud = null;
+var longitud = null;
 
 function onDeviceReady() {
 
@@ -12,6 +15,61 @@ function onDeviceReady() {
 	});
 
 	$("#enviaAlerta").click(function() {
-		alert("Debería enviar una alerta");
+		// Recuperamos el usuario de la bbdd
+		db.transaction(function(tx) {
+			tx.executeSql('SELECT * FROM smf WHERE id = ?', [1], function(tx, res) {
+				user = res.rows.item(0).name;
+			}, function(error) {
+				alert("Error en executeSql");
+			});
+		}, function(error) {
+			alert('Transaction ERROR: ' + error.message);
+		}, function() {
+			console.log('SELECT OK');
+		});
+
+		getPosition();
 	});
+
+	function getPosition() {
+		var options = {
+	  		enableHighAccuracy: true,
+	  		maximumAge: 3600000
+		}
+	
+		var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+		function onSuccess(position) {
+			latitud = position.coords.latitude;
+			longitud = position.coords.longitude;
+
+			alert(user);
+			alert(latitud);
+			alert(longitud);
+			// Así se busca en google maps con coordenadas: https://www.google.es/maps/@41.6474246,-0.9152144,15z
+			var localizacion = "https://www.google.es/maps/@" + latitud + "," + longitud + "z"
+
+			// Envíamos los datos pertinentes al servidor de SMF
+			$.ajax({
+				type: "POST",
+				url: "http://smfdatabase.esy.es/pruebaServidor/insertar.php",
+				data: ({name: user, location: localizacion})
+			})
+			.done(function(data) {
+				if (data == "1") {
+					alert("Aviso insertado");
+				} else {
+					alert("Error en el envío de datos");
+					alert(data);
+				}
+			})
+			.fail(function() {
+				alert("Error en el ajax");
+			});
+   		}
+
+		function onError(error) {
+			alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+		}
+	}
 }
